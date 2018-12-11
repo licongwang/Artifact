68a01b4f2ce1e8c80b0f664c2448505712e0e524
mida si <lcw@berkeley.edu>
2018-12-11 08:55:42 -0800

    added .gitignore

--- game/lane.py
+++ game/lane.py
@@ -5,7 +5,7 @@ class Lane(object):
         self.tower_hp = 40
         self.tower_broken = False
         self.improvement = []
-        self.player_units = [[0], [0]]
+        self.player_units = [[-1, 0], [-1, 0]]
         self.deployment_list = [[], []]
         pass
 
@@ -13,26 +13,29 @@ class Lane(object):
         self.deployment_list[player].append(unit)
 
     def deploy(self):
-        self.player_deploy(0)
-        self.player_deploy(1)
+        self.player_deploy_all(0)
+        self.player_deploy_all(1)
 
-    def player_deploy(self, player_num):
+    def player_deploy_all(self, player_num):
         self.gc.random.shuffle(self.deployment_list[player_num])
-        for unit in self.deployment_list[player_num]:
-            self.player_deploy_unit(player_num, unit)
+        for unit_to_deploy in self.deployment_list[player_num]:
+            units = self.player_units[player_num]
+            possible_index = [i for i in range(len(units)) if units[i] == 0]
+            chosen_index = self.gc.choice(possible_index)
+            self.player_deploy_unit(player_num, unit_to_deploy, chosen_index)
 
-    def player_deploy_unit(self, player_num, unit):
+    def player_deploy_unit(self, player_num, unit, position_index):
+        self.player_units[player_num][position_index] = unit
+        self.update_possible_slot(player_num)
+
+    def update_possible_slot(self, player_num):
         units = self.player_units[player_num]
-        possible_slot = [i for i in range(len(units)) if units[i] == 0]
-        chosen_slot = self.gc.choice(possible_slot)
-        self.player_units[player_num][possible_slot] = unit
-        extended = (possible_slot == 0 or possible_slot == len(self.player_units[player_num]) - 1 and
-        self.update_possible_slot()
+        if units[1] != 0:
+            units.insert(1, 0)
+        if units[-1] != 0:
+            units += [0]
+        self.player_units[player_num] = units
 
-    def update_possible_slot(self):
-        for units in self.player_units:
-            if 0 not in units:
-                units = [0] + units + [0]
 
     def battle(self):
         pass
